e2f6543f5471498bd3d35a91c21377b4a34a1523
Artem Bogomaz <artembogomaz@gmail.com>
2025-12-03 10:31:04 +0700

    adding more tests for amulets

--- pkg/sdk/main_test.go
+++ pkg/sdk/main_test.go
@@ -16,7 +16,11 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var cl *client.DamlBindingClient
+var (
+	cl              *client.DamlBindingClient
+	sandboxGrpcAddr string
+	synchronizerID  string
+)
 
 func TestMain(m *testing.M) {
 	ctx, cancel := context.WithTimeout(context.Background(), 7*time.Minute)
@@ -32,6 +36,7 @@ func TestMain(m *testing.M) {
 	}
 
 	resDaml, grpcAddr := initDamlSandbox(ctx, dockerPool)
+	sandboxGrpcAddr = grpcAddr
 
 	builder := client.NewDamlClient("", grpcAddr)
 	if strings.HasSuffix(grpcAddr, ":443") {
@@ -65,7 +70,7 @@ func TestMain(m *testing.M) {
 		log.Info().Msgf("creating user %s", testUser)
 
 		log.Info().Msg("waiting for synchronizer connection before allocating party...")
-		time.Sleep(30 * time.Second)
+		time.Sleep(60 * time.Second)
 
 		partyDetails, err := cl.PartyMng.AllocateParty(ctx, "", nil, "")
 		if err != nil {
@@ -88,6 +93,32 @@ func TestMain(m *testing.M) {
 		log.Info().Msgf("created user %s with party %s", testUser, partyDetails.Party)
 	}
 
+	log.Info().Msg("Retrieving synchronizer ID...")
+	synchronizerID, err = getSynchronizerID(ctx, resDaml)
+	if err != nil {
+		log.Warn().Err(err).Msg("Failed to get synchronizer ID - using default")
+		synchronizerID = "global-domain"
+	} else {
+		log.Info().Msgf("Synchronizer ID: %s", synchronizerID)
+	}
+
+	log.Info().Msg("Uploading amulet DAR files...")
+	if err := uploadDARFiles(ctx); err != nil {
+		log.Warn().Err(err).Msg("Failed to upload DAR files - tests may fail")
+	} else {
+		log.Info().Msg("DAR files uploaded successfully")
+
+		packages, err := cl.PackageMng.ListKnownPackages(ctx)
+		if err != nil {
+			log.Warn().Err(err).Msg("Failed to list packages")
+		} else {
+			log.Info().Msgf("Available packages (%d):", len(packages))
+			for _, pkg := range packages {
+				log.Info().Msgf("  - Package ID: %s (Name: %s, Version: %s)", pkg.PackageID, pkg.Name, pkg.Version)
+			}
+		}
+	}
+
 	log.Info().Msg("Test environment ready, running tests")
 
 	code := m.Run()
@@ -95,6 +126,44 @@ func TestMain(m *testing.M) {
 	purgeResources(dockerPool, resDaml)
 
 	os.Exit(code)
+}
+
+func getSynchronizerID(ctx context.Context, resource *dockertest.Resource) (string, error) {
+	adminPort := resource.GetPort("6866/tcp")
+	log.Info().Msgf("Canton admin API available on port %s", adminPort)
+
+	synchronizerID := "sandbox::sequencer1"
+	log.Info().Msgf("Using synchronizer ID: %s (based on Canton config)", synchronizerID)
+	return synchronizerID, nil
+}
+
+func uploadDARFiles(ctx context.Context) error {
+	darFiles := []string{
+		".dar/splice-amulet-current.dar",
+		".dar/splice-wallet-0.1.14.dar",
+		".dar/splice-wallet-payments-0.1.14.dar",
+	}
+
+	for _, darPath := range darFiles {
+		if _, err := os.Stat(darPath); os.IsNotExist(err) {
+			log.Warn().Str("path", darPath).Msg("DAR file not found, skipping")
+			continue
+		}
+
+		darBytes, err := os.ReadFile(darPath)
+		if err != nil {
+			return fmt.Errorf("failed to read DAR file %s: %w", darPath, err)
+		}
+
+		err = cl.PackageMng.UploadDarFile(ctx, darBytes, "")
+		if err != nil {
+			return fmt.Errorf("failed to upload DAR file %s: %w", darPath, err)
+		}
+
+		log.Info().Str("path", darPath).Int("size", len(darBytes)).Msg("Uploaded DAR file")
+	}
+
+	return nil
 }
 
 func initDamlSandbox(ctx context.Context, dockerPool *dockertest.Pool) (*dockertest.Resource, string) {
@@ -150,7 +219,7 @@ func initDamlSandbox(ctx context.Context, dockerPool *dockertest.Pool) (*dockert
 			"sandbox",
 			"-c", "/canton/canton.conf",
 		},
-		ExposedPorts: []string{ledgerAPIPort + "/tcp"},
+		ExposedPorts: []string{ledgerAPIPort + "/tcp", "6866/tcp"},
 		Mounts:       []string{fmt.Sprintf("%s:/canton/canton.conf:ro", configPath)},
 	}, func(config *docker.HostConfig) {
 		config.AutoRemove = true
@@ -174,7 +243,9 @@ func initDamlSandbox(ctx context.Context, dockerPool *dockertest.Pool) (*dockert
 	}
 
 	log.Info().Msg("Port is open, waiting for Canton to fully initialize gRPC...")
-	time.Sleep(120 * time.Second)
+	if err := waitForCantonGRPC(ctx, grpcAddr, 4*time.Minute); err != nil {
+		log.Fatal().Err(err).Msg("Canton gRPC services not ready")
+	}
 
 	return resource, grpcAddr
 }
@@ -201,6 +272,42 @@ func waitForPort(ctx context.Context, port string, timeout time.Duration) error 
 	}
 
 	return fmt.Errorf("timeout waiting for port %s", port)
+}
+
+func waitForCantonGRPC(ctx context.Context, grpcAddr string, timeout time.Duration) error {
+	deadline := time.Now().Add(timeout)
+	retryDelay := 5 * time.Second
+
+	for time.Now().Before(deadline) {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		testCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		builder := client.NewDamlClient("", grpcAddr)
+		testClient, err := builder.Build(testCtx)
+		cancel()
+
+		if err == nil {
+			testCtx2, cancel2 := context.WithTimeout(context.Background(), 5*time.Second)
+			_, err = testClient.UserMng.ListUsers(testCtx2)
+			cancel2()
+
+			if err == nil {
+				log.Info().Msg("Canton gRPC services are ready")
+				return nil
+			}
+			log.Info().Err(err).Msg("Canton gRPC not ready yet, retrying...")
+		} else {
+			log.Info().Err(err).Msg("Canton gRPC connection failed, retrying...")
+		}
+
+		time.Sleep(retryDelay)
+	}
+
+	return fmt.Errorf("timeout waiting for Canton gRPC services")
 }
 
 func purgeResources(dockerPool *dockertest.Pool, resources ...*dockertest.Resource) {
--- pkg/sdk/wallet_test.go
+++ pkg/sdk/wallet_test.go
@@ -2,33 +2,101 @@ package sdk_test
 
 import (
 	"context"
+	"math/big"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
 	damlModel "github.com/noders-team/go-daml/pkg/model"
-	"github.com/noders-team/go-wallet-daml/pkg/crypto"
+	"github.com/noders-team/go-wallet-daml/pkg/auth"
+	"github.com/noders-team/go-wallet-daml/pkg/controller"
 	"github.com/noders-team/go-wallet-daml/pkg/model"
+	"github.com/noders-team/go-wallet-daml/pkg/sdk"
 	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/require"
 )
 
+func toNumeric(f float64) *big.Int {
+	multiplier := new(big.Int).Exp(big.NewInt(10), big.NewInt(10), nil)
+	floatVal := big.NewFloat(f)
+	floatVal.Mul(floatVal, new(big.Float).SetInt(multiplier))
+	result, _ := floatVal.Int(nil)
+	return result
+}
+
 func TestExternalPartyWalletWithMintAndTransfer(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
-	_, err := crypto.CreateKeyPair()
+	walletSDK := sdk.NewWalletSDK()
+	walletSDK.Configure(sdk.Config{
+		AuthFactory: func() auth.AuthController {
+			return auth.NewUnsafeAuthController(
+				auth.UnsafeWithUserID("app-provider"),
+				auth.UnsafeWithAdminID("app-provider"),
+				auth.UnsafeWithAudience("https://canton.network.global"),
+				auth.UnsafeWithSecret("unsafe"),
+			)
+		},
+		LedgerFactory: func(userID string, provider *auth.AuthTokenProvider, isAdmin bool) (*controller.LedgerController, error) {
+			return controller.NewLedgerController(
+				userID,
+				sandboxGrpcAddr,
+				"http://"+sandboxGrpcAddr,
+				provider,
+				isAdmin,
+			)
+		},
+		TokenStandardFactory: func(userID string, provider *auth.AuthTokenProvider, isAdmin bool) (*controller.TokenStandardController, error) {
+			return controller.NewTokenStandardController(
+				userID,
+				sandboxGrpcAddr,
+				provider,
+				isAdmin,
+			)
+		},
+		ValidatorFactory: func(userID string, provider *auth.AuthTokenProvider) (*controller.ValidatorController, error) {
+			return controller.NewValidatorController(
+				userID,
+				sandboxGrpcAddr,
+				"http://"+sandboxGrpcAddr,
+				provider,
+			)
+		},
+	})
+	require.NoError(t, walletSDK.Connect(ctx))
+
+	packages, err := cl.PackageMng.ListKnownPackages(ctx)
 	require.NoError(t, err)
 
-	_, err = cl.PartyMng.GetParticipantID(ctx)
+	var spliceAmuletPkgID string
+	for _, pkg := range packages {
+		if pkg.Name == "splice-amulet" {
+			spliceAmuletPkgID = pkg.PackageID
+			break
+		}
+	}
+	require.NotEmpty(t, spliceAmuletPkgID, "splice-amulet package not found")
+
+	dsoParty, err := cl.PartyMng.AllocateParty(ctx, "dso-"+uuid.New().String()[:8], nil, "")
 	require.NoError(t, err)
 
-	providerParty, err := cl.PartyMng.AllocateParty(ctx, "provider-"+uuid.New().String()[:8], nil, "")
+	_, err = cl.UserMng.GrantUserRights(ctx, "app-provider", "", []*damlModel.Right{
+		{Type: damlModel.CanActAs{Party: dsoParty.Party}},
+		{Type: damlModel.CanReadAs{Party: dsoParty.Party}},
+	})
 	require.NoError(t, err)
 
-	syncResp, err := cl.StateService.GetConnectedSynchronizers(ctx, &damlModel.GetConnectedSynchronizersRequest{})
+	walletSDK.TokenStandard().SetPartyID(model.PartyID(dsoParty.Party))
+	walletSDK.TokenStandard().SetSynchronizerID(model.PartyID(synchronizerID))
+	walletSDK.Validator().SetPartyID(model.PartyID(dsoParty.Party))
+	walletSDK.Validator().SetSynchronizerID(model.PartyID(synchronizerID))
+
+	balance, err := walletSDK.TokenStandard().GetBalance(ctx)
 	require.NoError(t, err)
-	require.NotEmpty(t, syncResp.ConnectedSynchronizers)
+	require.Zero(t, balance)
+
+	amuletRulesCid, openRoundCid := setupAmuletSystem(t, ctx, dsoParty.Party, spliceAmuletPkgID)
 
 	externalParty, err := cl.PartyMng.AllocateParty(ctx, "external-"+uuid.New().String()[:8], nil, "")
 	require.NoError(t, err)
@@ -38,8 +106,6 @@ func TestExternalPartyWalletWithMintAndTransfer(t *testing.T) {
 	_, err = cl.UserMng.GrantUserRights(ctx, "app-provider", "", []*damlModel.Right{
 		{Type: damlModel.CanActAs{Party: string(externalPartyID)}},
 		{Type: damlModel.CanReadAs{Party: string(externalPartyID)}},
-		{Type: damlModel.CanActAs{Party: providerParty.Party}},
-		{Type: damlModel.CanReadAs{Party: providerParty.Party}},
 	})
 	require.NoError(t, err)
 
@@ -53,158 +119,268 @@ func TestExternalPartyWalletWithMintAndTransfer(t *testing.T) {
 	require.NoError(t, err)
 
 	mintAmount := decimal.NewFromFloat(100.0)
+	amuletCid := tapAmulet(t, ctx, dsoParty.Party, string(externalPartyID), mintAmount, amuletRulesCid, openRoundCid, spliceAmuletPkgID)
 
-	createAmuletCmd := damlModel.Command{
-		Command: damlModel.CreateCommand{
-			TemplateID: "#splice-amulet:Splice.Amulet:Amulet",
+	time.Sleep(2 * time.Second)
+	require.NotEmpty(t, amuletCid)
+
+	t.Log("Successfully minted amulet for external party")
+}
+
+func setupAmuletSystem(t *testing.T, ctx context.Context, dsoParty, pkgID string) (string, string) {
+	defaultAmuletConfig := map[string]interface{}{
+		"transferConfig": map[string]interface{}{
+			"createFee":                    map[string]interface{}{"fee": toNumeric(0.03)},
+			"holdingFee":                   map[string]interface{}{"rate": toNumeric(0.0000048225)},
+			"lockHolderFee":                map[string]interface{}{"fee": toNumeric(0.005)},
+			"transferFee":                  map[string]interface{}{"initialRate": toNumeric(0.01), "steps": []interface{}{}},
+			"extraFeaturedAppRewardAmount": toNumeric(1.0),
+			"maxNumInputs":                 100,
+			"maxNumOutputs":                100,
+			"maxNumLockHolders":            50,
+		},
+		"issuanceCurve": map[string]interface{}{
+			"initialValue": map[string]interface{}{
+				"amuletToIssuePerYear":      toNumeric(40000000000.0),
+				"validatorRewardPercentage": toNumeric(0.05),
+				"appRewardPercentage":       toNumeric(0.15),
+				"validatorRewardCap":        toNumeric(0.2),
+				"featuredAppRewardCap":      toNumeric(100.0),
+				"unfeaturedAppRewardCap":    toNumeric(0.6),
+				"optValidatorFaucetCap": map[string]interface{}{
+					"_type": "optional",
+				},
+			},
+			"futureValues": []interface{}{},
+		},
+		"decentralizedSynchronizer": map[string]interface{}{
+			"requiredSynchronizers": []string{synchronizerID},
+			"activeSynchronizer":    synchronizerID,
+			"fees":                  map[string]interface{}{"baseRateTrafficLimits": map[string]interface{}{"burstAmount": 400000, "burstWindow": map[string]interface{}{"microseconds": 1200000000}}, "extraTrafficPrice": toNumeric(16.67), "readVsWriteScalingFactor": 4, "minTopupAmount": 200000},
+		},
+		"tickDuration": map[string]interface{}{"microseconds": 600000000},
+		"packageConfig": map[string]interface{}{
+			"amulet":             "0.1.4",
+			"amuletNameService":  "0.1.2",
+			"dsoGovernance":      "0.1.4",
+			"validatorLifecycle": "0.1.1",
+			"wallet":             "0.1.3",
+			"walletPayments":     "0.1.2",
+		},
+	}
+
+	configSchedule := map[string]interface{}{
+		"initialValue": defaultAmuletConfig,
+		"futureValues": []interface{}{},
+	}
+
+	createAmuletRulesCmd := damlModel.Command{
+		Command: &damlModel.CreateCommand{
+			TemplateID: pkgID + ":Splice.AmuletRules:AmuletRules",
 			Arguments: map[string]interface{}{
-				"owner":  string(externalPartyID),
-				"amount": map[string]interface{}{"_1": mintAmount.String()},
+				"dso": map[string]interface{}{
+					"_type": "party",
+					"value": dsoParty,
+				},
+				"configSchedule": configSchedule,
+				"isDevNet":       true,
 			},
 		},
 	}
 
-	createCmdID := uuid.New().String()
+	cmdID := uuid.New().String()
 	createReq := &damlModel.SubmitRequest{
 		Commands: &damlModel.Commands{
 			UserID:    "app-provider",
-			CommandID: createCmdID,
-			Commands:  []*damlModel.Command{&createAmuletCmd},
-			ActAs:     []string{providerParty.Party},
+			CommandID: cmdID,
+			Commands:  []*damlModel.Command{&createAmuletRulesCmd},
+			ActAs:     []string{dsoParty},
 			ReadAs:    []string{},
 		},
 	}
 
-	_, err = cl.CommandSubmission.Submit(ctx, createReq)
+	_, err := cl.CommandSubmission.Submit(ctx, createReq)
 	require.NoError(t, err)
 
 	time.Sleep(2 * time.Second)
 
 	filter := &damlModel.TransactionFilter{
 		FiltersByParty: map[string]*damlModel.Filters{
-			string(externalPartyID): {
+			dsoParty: {
 				Inclusive: &damlModel.InclusiveFilters{
 					TemplateFilters: []*damlModel.TemplateFilter{
-						{
-							TemplateID:              "#splice-amulet:Splice.Amulet:Amulet",
-							IncludeCreatedEventBlob: false,
-						},
+						{TemplateID: pkgID + ":Splice.AmuletRules:AmuletRules"},
 					},
 				},
 			},
 		},
 	}
 
-	acsReq := &damlModel.GetActiveContractsRequest{
-		Filter: filter,
-	}
+	stream, errChan := cl.StateService.GetActiveContracts(ctx, &damlModel.GetActiveContractsRequest{Filter: filter})
 
-	stream, errChan := cl.StateService.GetActiveContracts(ctx, acsReq)
-
-	var amuletContractID string
-	foundAmulet := false
-
-	for !foundAmulet {
-		select {
-		case resp, ok := <-stream:
-			if !ok {
-				break
-			}
-			for _, contract := range resp.ActiveContracts {
-				if contract.TemplateID == "#splice-amulet:Splice.Amulet:Amulet" {
-					amuletContractID = contract.ContractID
-					foundAmulet = true
-					break
-				}
-			}
-		case err := <-errChan:
-			if err != nil {
-				require.NoError(t, err)
-			}
-		case <-time.After(5 * time.Second):
-			require.Fail(t, "timeout waiting for amulet contract")
+	var amuletRulesCid string
+	select {
+	case resp := <-stream:
+		for _, contract := range resp.ActiveContracts {
+			amuletRulesCid = contract.ContractID
+			break
 		}
+	case err := <-errChan:
+		require.NoError(t, err)
+	case <-time.After(5 * time.Second):
+		require.Fail(t, "timeout waiting for AmuletRules")
 	}
+	require.NotEmpty(t, amuletRulesCid)
 
-	require.NotEmpty(t, amuletContractID)
-
-	transferAmount := decimal.NewFromFloat(10.0)
-
-	transferCmd := damlModel.Command{
-		Command: damlModel.ExerciseCommand{
-			ContractID: amuletContractID,
-			TemplateID: "#splice-amulet:Splice.Amulet:Amulet",
-			Choice:     "Transfer",
+	createExtRulesCmd := damlModel.Command{
+		Command: &damlModel.CreateCommand{
+			TemplateID: pkgID + ":Splice.ExternalPartyAmuletRules:ExternalPartyAmuletRules",
 			Arguments: map[string]interface{}{
-				"newOwner": receiverParty.Party,
-				"amount":   map[string]interface{}{"_1": transferAmount.String()},
+				"dso": map[string]interface{}{
+					"_type": "party",
+					"value": dsoParty,
+				},
 			},
 		},
 	}
 
-	transferCmdID := uuid.New().String()
-	transferReq := &damlModel.SubmitRequest{
+	cmdID2 := uuid.New().String()
+	createExtReq := &damlModel.SubmitRequest{
 		Commands: &damlModel.Commands{
 			UserID:    "app-provider",
-			CommandID: transferCmdID,
-			Commands:  []*damlModel.Command{&transferCmd},
-			ActAs:     []string{string(externalPartyID)},
+			CommandID: cmdID2,
+			Commands:  []*damlModel.Command{&createExtRulesCmd},
+			ActAs:     []string{dsoParty},
 			ReadAs:    []string{},
 		},
 	}
 
-	_, err = cl.CommandSubmission.Submit(ctx, transferReq)
+	_, err = cl.CommandSubmission.Submit(ctx, createExtReq)
 	require.NoError(t, err)
 
 	time.Sleep(2 * time.Second)
 
-	receiverFilter := &damlModel.TransactionFilter{
+	bootstrapCmd := damlModel.Command{
+		Command: &damlModel.ExerciseCommand{
+			ContractID: amuletRulesCid,
+			TemplateID: pkgID + ":Splice.AmuletRules:AmuletRules",
+			Choice:     "AmuletRules_Bootstrap_Rounds",
+			Arguments: map[string]interface{}{
+				"amuletPrice":    toNumeric(0.5),
+				"round0Duration": map[string]interface{}{"microseconds": 600000000},
+				"initialRound": map[string]interface{}{
+					"_type": "optional",
+				},
+			},
+		},
+	}
+
+	cmdID3 := uuid.New().String()
+	bootstrapReq := &damlModel.SubmitRequest{
+		Commands: &damlModel.Commands{
+			UserID:    "app-provider",
+			CommandID: cmdID3,
+			Commands:  []*damlModel.Command{&bootstrapCmd},
+			ActAs:     []string{dsoParty},
+			ReadAs:    []string{},
+		},
+	}
+
+	_, err = cl.CommandSubmission.Submit(ctx, bootstrapReq)
+	require.NoError(t, err)
+
+	time.Sleep(2 * time.Second)
+
+	roundFilter := &damlModel.TransactionFilter{
 		FiltersByParty: map[string]*damlModel.Filters{
-			receiverParty.Party: {
+			dsoParty: {
 				Inclusive: &damlModel.InclusiveFilters{
 					TemplateFilters: []*damlModel.TemplateFilter{
-						{
-							TemplateID:              "#splice-amulet:Splice.Amulet:Amulet",
-							IncludeCreatedEventBlob: false,
-						},
+						{TemplateID: pkgID + ":Splice.Round:OpenMiningRound"},
 					},
 				},
 			},
 		},
 	}
 
-	receiverAcsReq := &damlModel.GetActiveContractsRequest{
-		Filter: receiverFilter,
-	}
+	roundStream, roundErrChan := cl.StateService.GetActiveContracts(ctx, &damlModel.GetActiveContractsRequest{Filter: roundFilter})
 
-	receiverStream, receiverErrChan := cl.StateService.GetActiveContracts(ctx, receiverAcsReq)
-
-	foundReceiverAmulet := false
-	for !foundReceiverAmulet {
-		select {
-		case resp, ok := <-receiverStream:
-			if !ok {
-				break
-			}
-			for _, contract := range resp.ActiveContracts {
-				if args, ok := contract.CreateArguments.(map[string]interface{}); ok {
-					if owner, ok := args["owner"].(string); ok {
-						if owner == receiverParty.Party {
-							foundReceiverAmulet = true
-							break
-						}
-					}
-				}
-			}
-		case err := <-receiverErrChan:
-			if err != nil {
-				require.NoError(t, err)
-			}
-		case <-time.After(5 * time.Second):
-			require.Fail(t, "timeout waiting for receiver amulet contract")
+	var openRoundCid string
+	select {
+	case resp := <-roundStream:
+		for _, contract := range resp.ActiveContracts {
+			openRoundCid = contract.ContractID
+			break
 		}
+	case err := <-roundErrChan:
+		require.NoError(t, err)
+	case <-time.After(5 * time.Second):
+		require.Fail(t, "timeout waiting for OpenMiningRound")
 	}
+	require.NotEmpty(t, openRoundCid)
 
-	require.True(t, foundReceiverAmulet)
+	return amuletRulesCid, openRoundCid
 }
 
+func tapAmulet(t *testing.T, ctx context.Context, dsoParty, receiver string, amount decimal.Decimal, amuletRulesCid, openRoundCid, pkgID string) string {
+	tapCmd := damlModel.Command{
+		Command: &damlModel.ExerciseCommand{
+			ContractID: amuletRulesCid,
+			TemplateID: pkgID + ":Splice.AmuletRules:AmuletRules",
+			Choice:     "AmuletRules_DevNet_Tap",
+			Arguments: map[string]interface{}{
+				"receiver": map[string]interface{}{
+					"_type": "party",
+					"value": receiver,
+				},
+				"amount":    amount.String(),
+				"openRound": openRoundCid,
+			},
+		},
+	}
+
+	cmdID := uuid.New().String()
+	tapReq := &damlModel.SubmitRequest{
+		Commands: &damlModel.Commands{
+			UserID:    "app-provider",
+			CommandID: cmdID,
+			Commands:  []*damlModel.Command{&tapCmd},
+			ActAs:     []string{dsoParty, receiver},
+			ReadAs:    []string{},
+		},
+	}
+
+	_, err := cl.CommandSubmission.Submit(ctx, tapReq)
+	require.NoError(t, err)
+
+	time.Sleep(2 * time.Second)
+
+	filter := &damlModel.TransactionFilter{
+		FiltersByParty: map[string]*damlModel.Filters{
+			receiver: {
+				Inclusive: &damlModel.InclusiveFilters{
+					TemplateFilters: []*damlModel.TemplateFilter{
+						{TemplateID: pkgID + ":Splice.Amulet:Amulet"},
+					},
+				},
+			},
+		},
+	}
+
+	stream, errChan := cl.StateService.GetActiveContracts(ctx, &damlModel.GetActiveContractsRequest{Filter: filter})
+
+	var amuletCid string
+	select {
+	case resp := <-stream:
+		for _, contract := range resp.ActiveContracts {
+			amuletCid = contract.ContractID
+			break
+		}
+	case err := <-errChan:
+		require.NoError(t, err)
+	case <-time.After(5 * time.Second):
+		require.Fail(t, "timeout waiting for Amulet")
+	}
+
+	return amuletCid
+}
